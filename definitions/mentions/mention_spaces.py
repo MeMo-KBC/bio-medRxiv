bed030b56f4e084d1e3021d4e0d80a75ca1b52cd
Nils Grote <nils.grote@smail.th-koeln.de>
2023-04-17 12:52:41 +0000

    added folders for definitions

---
+++ definitions/mentions/mention_spaces.py
@@ -0,0 +1,3 @@
+from fonduer.candidates import MentionNgrams
+
+nameFull_ngrams = MentionNgrams(n_min=1, n_max=2)
---
+++ definitions/mentions/mention_subclasses.py
@@ -0,0 +1,5 @@
+from fonduer.candidates.models import mention_subclass
+
+NameFull = mention_subclass("NameFull")
+NameAbbr = mention_subclass("NameAbbr")
+
