c3ea3d8737be93d60024ccfb7033f6b82a377272
Nils Grote <nils.grote@smail.th-koeln.de>
2023-04-18 12:57:24 +0000

    fixed typo

--- pipeline/session.py
+++ pipeline/session.py
@@ -3,4 +3,4 @@ from fonduer.meta import Meta
 
 def get_session(db_name: str):
     conn_str = 'postgresql://localhost:5432/' + db_name
-    return Meta.init(conn_str=conn_str).Session()
+    return Meta.init(conn_string=conn_str).Session()
