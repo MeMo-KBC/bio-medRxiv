2e7bd48a8c008fbc55005ec826375cafecc8f4f9
Jannick Kracht <jannick.kracht@smail.th-koeln.de>
2023-06-07 19:55:35 +0000

    add throttler Abb in Task

--- MeMoKBC/src/MeMoKBC/scripts/apply_lf.py
+++ MeMoKBC/src/MeMoKBC/scripts/apply_lf.py
@@ -2,12 +2,12 @@ from fonduer.supervision import Labeler
 from fonduer.parser.models import Document
 
 from MeMoKBC.definitions.candidates import NameFullAbbr, NameAbbrTask
-from MeMoKBC.pipeline.lfs.dummy_lfs import dummy_lfs
+from MeMoKBC.pipeline.lfs.name_short_task_lfs import short_task_lfs
 from MeMoKBC.pipeline.lfs.name_short_long_lfs import short_long_lfs
 
 def apply_lf(session, parallel: int=12):
     labeler = Labeler(session, [NameFullAbbr, NameAbbrTask])
 
-    labeler.apply(split=0, lfs=[short_long_lfs, dummy_lfs], train=True, parallelism=parallel)
-    labeler.apply(split=1, lfs=[short_long_lfs, dummy_lfs], parallelism=parallel)
-    labeler.apply(split=2, lfs=[short_long_lfs, dummy_lfs], parallelism=parallel)
+    labeler.apply(split=0, lfs=[short_long_lfs, short_task_lfs], train=True, parallelism=parallel)
+    labeler.apply(split=1, lfs=[short_long_lfs, short_task_lfs], parallelism=parallel)
+    labeler.apply(split=2, lfs=[short_long_lfs, short_task_lfs], parallelism=parallel)
