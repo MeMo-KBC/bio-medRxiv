2fc2d9ed97b4402a2ee911c569536b6e0610ced8
Jannick Kracht <jannick.kracht@smail.th-koeln.de>
2023-06-07 14:29:14 +0000

    put lfs of NameAbbTask in seperate folder

---
+++ MeMoKBC/src/MeMoKBC/pipeline/lfs/name_short_task_lfs.py
@@ -0,0 +1,40 @@
+from snorkel.labeling import labeling_function
+
+ABSTAIN = -1
+FALSE = 0
+TRUE = 1
+
+@labeling_function()
+def lf_length_more_than_three_words(c):
+    sentence = c[1].context.get_span().split(" ")
+    if len(sentence) > 3:
+        # print("1")
+        return ABSTAIN
+    else:
+        # print("2")
+        return FALSE
+
+@labeling_function()
+def lf_name_short_in_first_words(c):
+    name = c[0].context.get_span()
+    sentence = c[1].context.get_span().split(" ")
+    i = 1
+    for word in sentence:
+        if name.lower() == word.lower(): # was ist mit sonderzeichen? word stemmen und vergleichen?
+            if i <= 3:
+                # print("3")
+                return TRUE
+            else:
+                # print("4")
+                return ABSTAIN
+        i+=1   
+    return ABSTAIN
+     
+        
+    
+
+
+NameAbb_Task_lfs = [
+    lf_length_more_than_three_words,
+    lf_name_short_in_first_words,
+]
--- MeMoKBC/src/MeMoKBC/scripts/apply_lf.py
+++ MeMoKBC/src/MeMoKBC/scripts/apply_lf.py
@@ -2,12 +2,12 @@ from fonduer.supervision import Labeler
 from fonduer.parser.models import Document
 
 from MeMoKBC.definitions.candidates import NameFullAbbr, NameAbbrTask
+from MeMoKBC.pipeline.lfs.name_short_task_lfs import NameAbb_Task_lfs
 from MeMoKBC.pipeline.lfs.dummy_lfs import dummy_lfs
-
 
 def apply_lf(session, parallel: int=12):
     labeler = Labeler(session, [NameFullAbbr, NameAbbrTask])
 
-    labeler.apply(split=0, lfs=[None, dummy_lfs], train=True, parallelism=parallel)
-    # labeler.apply(split=1, lfs=[dummy_lfs, dummy_lfs], parallelism=parallel)
-    # labeler.apply(split=2, lfs=[dummy_lfs, dummy_lfs], parallelism=parallel)
+    labeler.apply(split=0, lfs=[dummy_lfs, NameAbb_Task_lfs], train=True, parallelism=parallel)
+    labeler.apply(split=1, lfs=[dummy_lfs, dummy_lfs], parallelism=parallel)
+    labeler.apply(split=2, lfs=[dummy_lfs, dummy_lfs], parallelism=parallel)
