af9a754099b96bb54f1e626d6807113985c91f99
Jannick Kracht <jannick.kracht@smail.th-koeln.de>
2023-06-14 21:55:33 +0000

    apply name_short_long lf to name_abb_task, add one idea for lf as a comment

--- MeMoKBC/src/MeMoKBC/pipeline/lfs/dummy_lfs.py
+++ MeMoKBC/src/MeMoKBC/pipeline/lfs/dummy_lfs.py
@@ -27,9 +27,32 @@ def dummy_lf_randi(c):
     else:
         return FALSE
 
+## ideen für mehr lfs
+
+
+# NameAbbTask
+# copy from name_short_long lfs and add task mention
+@labeling_function()
+def candidate_mentions_outside_half_percentile(c):
+    '''Checks if name short is in the lower half of the document'''
+    matcher_name_short = c[0]
+    matcher_task = c[1]
+    short_vert_percentile = get_page_vert_percentile(matcher_name_short)
+    if short_vert_percentile >= 0.5 and matcher_task >= 0.5:
+        return TRUE
+    else:
+        return ABSTAIN
+
+# mögliche fehlerquellen
+# Abkürzung ist keine Namensabkürzung einer Person
+# ist es möglich, auf nameshortlong- candidaten zuzugreifen und zu prüfen ob es einen long name zu dem kurzel gibt? - falls nicht -> false?
+
+
+# NameShortLong
+
+
 
 dummy_lfs = [
-    dummy_lf_rand,
-    dummy_lf_rando,
-    dummy_lf_randi
+    candidate_mentions_outside_half_percentile,
+
 ]
